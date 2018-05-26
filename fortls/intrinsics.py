948776f2e158175d71607611f3a9a2a1e6959e1a
Chris Hansen <hansec@uw.edu>
2018-05-26 14:58:10 -0400

    Add remaining intrinsic functions up to 2003 standard

--- fortls/intrinsics.py
+++ fortls/intrinsics.py
@@ -242,6 +242,8 @@ def get_intrinsics():
                               "NAME=name,VALUE=value,LENGTH=length,STATUS=status,TRIM_NAME=trim_name"),
         fortran_intrinsic_obj("HUGE", 3, "HUGE(X) returns the largest number that "
                               "is not an infinity in the model of the type of X.", "X"),
+        fortran_intrinsic_obj("HYPOT", 3, "HYPOT(X,Y)  is the Euclidean distance function "
+                              "without undue underflow or overflow.", "X,Y"),
         fortran_intrinsic_obj("IACHAR", 3, "IACHAR(C,KIND=kind) returns the code for the ASCII character "
                               "in the first character position of C.", "I,KIND=kind"),
         fortran_intrinsic_obj("IALL", 3, "IALL(MASK,DIM=dim) reduces with bitwise AND the elements "
@@ -249,16 +251,50 @@ def get_intrinsics():
         fortran_intrinsic_obj("IAND", 3, "IAND(I,J) Bitwise logical AND.", "I,J"),
         fortran_intrinsic_obj("IANY", 3, "IANY(MASK,DIM=dim) reduces with bitwise OR the elements "
                               "of ARRAY along dimension DIM.", "MASK,DIM=dim"),
+        fortran_intrinsic_obj("IBCLR", 3, "IBCLR(I,POS) returns the value of I with the bit at position "
+                              "POS set to zero.", "I,POS"),
+        fortran_intrinsic_obj("IBITS", 3, "IBITS(I,POS,LEN) extracts a field of length LEN from I, "
+                              "starting from bit position POS and extending left for LEN bits.", "I,POS,LEN"),
+        fortran_intrinsic_obj("IBSET", 3, "IBSET(I,POS) returns the value of I with the bit at position "
+                              "POS set to one.", "I,POS"),
         fortran_intrinsic_obj("ICHAR", 3, "ICHAR(C,KIND=kind) returns the code for the character "
                               "in the first character position of C in the system's native character set.",
                               "I,KIND=kind"),
         fortran_intrinsic_obj("IEOR", 3, "IEOR(I,J) Bitwise logical exclusive OR.", "I,J"),
         fortran_intrinsic_obj("IMAGE_INDEX", 3, "IMAGE_INDEX(COARRAY,SUB) returns the image index "
                               "belonging to a cosubscript.", "COARRAY,SUB"),
+        fortran_intrinsic_obj("INDEX", 3, "INDEX(STRING,SUBSTRING,BACK=back,KIND=kind) returns the "
+                              "position of the start of the first occurrence of string SUBSTRING as "
+                              "a substring in STRING, counting from one.", "STRING,SUBSTRING,BACK=back,KIND=kind"),
+        fortran_intrinsic_obj("IOR", 3, "IOR(I,J) Bitwise logical inclusive OR.", "I,J"),
+        fortran_intrinsic_obj("IPARITY", 3, "IPARITY(ARRAY,DIM=dim,MASK=mask) reduces with bitwise XOR "
+                              "(exclusive or) the elements of ARRAY along dimension DIM if the "
+                              "corresponding element in MASK is TRUE.", "ARRAY,DIM=dim,MASK=mask"),
+        fortran_intrinsic_obj("IS_IOSTAT_END", 3, "IS_IOSTAT_END(I) tests whether the variable I has the "
+                              "value of the I/O status 'end of file'", "I"),
+        fortran_intrinsic_obj("IS_IOSTAT_EOR", 3, "IS_IOSTAT_EOR(I) tests whether the variable I has the "
+                              "value of the I/O status 'end of record'", "I"),
+        fortran_intrinsic_obj("ISHIFT", 3, "ISHIFT(I,SHIFT) returns a value corresponding to I with "
+                              "all of the bits shifted SHIFT places", "I,SHIFT"),
+        fortran_intrinsic_obj("ISHIFTC", 3, "ISHIFTC(I,SHIFT,SIZE=size) returns a value corresponding "
+                              "to I with the rightmost SIZE bits shifted circularly SHIFT places; that "
+                              "is, bits shifted out one end are shifted into the opposite end.", "I,SHIFT,SIZE=size"),
+        fortran_intrinsic_obj("KIND", 3, "KIND(X) returns the kind value of the entity X.", "X"),
+        fortran_intrinsic_obj("LBOUND", 3, "LBOUND(ARRAY,DIM=dim,KIND=kind) returns the lower bounds "
+                              "of an array, or a single lower bound along the DIM dimension.",
+                              "ARRAY,DIM=dim,KIND=kind"),
         fortran_intrinsic_obj("LEN", 3, "LEN(STRING,KIND=kind) returns the length of a character "
                               "string.", "STRING,KIND=kind"),
         fortran_intrinsic_obj("LEN_TRIM", 3, "LEN_TRIM(STRING,KIND=kind) returns the length of a character "
                               "string, ignoring any trailing blanks.", "STRING,KIND=kind"),
+        fortran_intrinsic_obj("LGE", 3, "LGE(STRING_A,STRING_B) determines whether one string is "
+                              "lexically greater than or equal to another string.", "STRING_A,STRING_B"),
+        fortran_intrinsic_obj("LGT", 3, "LGT(STRING_A,STRING_B) determines whether one string is "
+                              "lexically greater than another string.", "STRING_A,STRING_B"),
+        fortran_intrinsic_obj("LLE", 3, "LLE(STRING_A,STRING_B) determines whether one string is "
+                              "lexically less than or equal to another string.", "STRING_A,STRING_B"),
+        fortran_intrinsic_obj("LLT", 3, "LLT(STRING_A,STRING_B) determines whether one string is "
+                              "lexically less than another string.", "STRING_A,STRING_B"),
         fortran_intrinsic_obj("LOG", 3, "LOG(X) computes the natural logarithm of X, i.e. the "
                               "logarithm to the base e.", "X"),
         fortran_intrinsic_obj("LOG10", 3, "LOG10(X) computes the base 10 logarithm of X.", "X"),
@@ -266,6 +302,8 @@ def get_intrinsics():
                               "multiplication on numeric or logical arguments.", "MATRIX_A,MATRIX_B"),
         fortran_intrinsic_obj("MAX", 3, "MAX(A1,A2,...) returns the argument with the smallest "
                               "(most negative) value.", "A1,A2"),
+        fortran_intrinsic_obj("MAXEXPONENT", 3, "MAXEXPONENT(X) returns the maximum exponent in "
+                              "the model of the type of X.", "X"),
         fortran_intrinsic_obj("MAXLOC", 3, "MAXLOC(ARRAY,DIM=dim,MASK=mask,KIND=kind,BACK=back) "
                               "determines the location of the element in the array with the maximum "
                               "value.",
@@ -273,8 +311,12 @@ def get_intrinsics():
         fortran_intrinsic_obj("MAXVAL", 3, "MAXVAL(ARRAY,DIM=dim,MASK=mask) "
                               "determines the maximum value of the elements in an array.",
                               "ARRAY,DIM=dim,MASK=mask,KIND=kind,BACK=back"),
+        fortran_intrinsic_obj("MERGE", 3, "MERGE(TSOURCE,FSOURCE,MASK) select values from two "
+                              "arrays according to a logical mask.", "TSOURCE,FSOURCE,MASK"),
         fortran_intrinsic_obj("MIN", 3, "MIN(A1,A2,...) returns the argument with the largest (most "
                               "positive) value.", "A1,A2"),
+        fortran_intrinsic_obj("MINEXPONENT", 3, "MINEXPONENT(X) returns the minimum exponent in "
+                              "the model of the type of X.", "X"),
         fortran_intrinsic_obj("MINLOC", 3, "MINLOC(ARRAY,DIM=dim,MASK=mask,KIND=kind,BACK=back) "
                               "determines the location of the element in the array with the minimum "
                               "value.",
@@ -283,15 +325,61 @@ def get_intrinsics():
                               "determines the minimum value of the elements in an array.",
                               "ARRAY,DIM=dim,MASK=mask,KIND=kind,BACK=back"),
         fortran_intrinsic_obj("MOD", 3, "MOD(A,P) computes the remainder of the division of A by P.", "A,P"),
+        fortran_intrinsic_obj("MODULO", 3, "MODULO(A,P) computes the A modulo P.", "A,P"),
+        fortran_intrinsic_obj("MOVE_ALLOC", 3, "MOVE_ALLOC(FROM,TO) moves the allocation from FROM to TO.",
+                              "FROM,TO"),
+        fortran_intrinsic_obj("MVBITS", 3, "MVBITS(FROM,FROMPOS,LEN,TO,TOPOS) moves LEN bits from "
+                              "positions FROMPOS through FROMPOS+LEN-1 of FROM to positions TOPOS "
+                              "through TOPOS+LEN-1 of TO.", "FROM,TO"),
+        fortran_intrinsic_obj("NEAREST", 3, "NEAREST(X,S) returns the processor-representable "
+                              "number nearest to X in the direction indicated by the sign of S.",
+                              "X,S"),
+        fortran_intrinsic_obj("NEW_LINE", 3, "NEW_LINE(C) returns the new-line character.", "C"),
+        fortran_intrinsic_obj("NINT", 3, "NINT(A,KIND=kind) rounds its argument to the nearest whole number.",
+                              "A,KIND=kind"),
+        fortran_intrinsic_obj("NOT", 3, "NOT(I) returns the bitwise Boolean inverse of I.", "I"),
+        fortran_intrinsic_obj("NULL", 3, "NULL() returns a disassociated pointer.", ""),
+        fortran_intrinsic_obj("PACK", 3, "PACK(ARRAY,MASK,VECTOR=vector) stores the elements "
+                              "of ARRAY in an array of rank one.", "A,KIND=kind"),
+        fortran_intrinsic_obj("PRECISION", 3, "PRECISION(X) returns the decimal precision in the "
+                              "model of the type of X.", "X"),
         fortran_intrinsic_obj("PRESENT", 3, "PRESENT(A) determines whether an optional dummy argument is present.",
                               "A"),
         fortran_intrinsic_obj("PRODUCT", 3, "PRODUCT(ARRAY,DIM=dim,MASK=mask) multiplies the elements "
                               "of ARRAY along dimension DIM if the corresponding element in MASK is TRUE.",
                               "ARRAY,DIM=dim,MASK=mask"),
+        fortran_intrinsic_obj("RADIX", 3, "RADIX(X) returns the base of the model representing the entity X.",
+                              "X"),
         fortran_intrinsic_obj("RANDOM_NUMBER", 2, "RANDOM_NUMBER(HARVEST) returns a single "
                               "pseudorandom number or an array of pseudorandom numbers.", "HARVEST"),
+        fortran_intrinsic_obj("RANDOM_SEED", 2, "RANDOM_SEED(SIZE=size,PUT=put,GET=get) restarts or "
+                              "queries the state of the pseudorandom number generator used by RANDOM_NUMBER.",
+                              "SIZE=size,PUT=put,GET=get"),
+        fortran_intrinsic_obj("RANGE", 3, "RANGE(X) returns the decimal exponent range in the model "
+                              "of the type of X.", "X"),
+        fortran_intrinsic_obj("REPEAT", 3, "REPEAT(STRING,NCOPIES) concatenates NCOPIES copies of a string.",
+                              "STRING,NCOPIES"),
         fortran_intrinsic_obj("RESHAPE", 3, "RESHAPE(SOURCE,SHAPE,PAD=pad,ORDER=order) reshapes SOURCE "
                               "to correspond to SHAPE.", "SOURCE,SHAPE,PAD=pad,ORDER=order"),
+        fortran_intrinsic_obj("RRSPACING", 3, "RRSPACING(X) returns the reciprocal of the relative spacing "
+                              "of model numbers near X.", "X"),
+        fortran_intrinsic_obj("SAME_TYPE_AS", 3, "SAME_TYPE_AS(A,B) query dynamic types for equality.",
+                              "A,B"),
+        fortran_intrinsic_obj("SCALE", 3, "SCALE(X,I) returns X * RADIX(X)**I.",
+                              "X,I"),
+        fortran_intrinsic_obj("SCAN", 3, "SCAN(STRING,SET,BACK=back,KIND=kind) scans a STRING for "
+                              "any of the characters in a SET of characters.", "SOURCE,KIND=kind"),
+        fortran_intrinsic_obj("SELECTED_CHAR_KIND", 3, "SELECTED_CHAR_KIND(NAME) returns the kind "
+                              "value for the character set named NAME, if a character set with "
+                              "such a name is supported, or -1 otherwise.", "NAME"),
+        fortran_intrinsic_obj("SELECTED_INT_KIND", 3, "SELECTED_INT_KIND(R) return the kind "
+                              "value of the smallest integer type that can represent all values "
+                              "ranging from -10^R (exclusive) to 10^R (exclusive).", "R"),
+        fortran_intrinsic_obj("SELECTED_REAL_KIND", 3, "SELECTED_REAL_KIND(P,R) returns the kind "
+                              "value of a real data type with decimal precision of at least P "
+                              "digits, exponent range of at least R, and with a radix of RADIX.", "P,R"),
+        fortran_intrinsic_obj("SET_EXPONENT", 3, "SET_EXPONENT(X,I) returns the real number "
+                              "whose fractional part is that that of X and whose exponent part is I.", "X,I"),
         fortran_intrinsic_obj("SHAPE", 3, "SHAPE(SOURCE,KIND=kind) determines the shape of an array.",
                               "SOURCE,KIND=kind"),
         fortran_intrinsic_obj("SIGN", 3, "SIGN(A,B) returns the value of A with the sign of B.", "A,B"),
@@ -300,16 +388,36 @@ def get_intrinsics():
         fortran_intrinsic_obj("SIZE", 3, "SIZE(ARRAY,DIM=dim,KIND=kind) determines the extent of ARRAY "
                               "along a specified dimension DIM, or the total number of elements in ARRAY "
                               "if DIM is absent.", "ARRAY,DIM=dim,KIND=kind"),
+        fortran_intrinsic_obj("SPACING", 3, "SPACING(X) determines the distance between the argument X "
+                              "and the nearest adjacent number of the same type.", "X"),
+        fortran_intrinsic_obj("SPREAD", 3, "SPREAD(SOURCE,DIM,NCOPIES) replicates a SOURCE array "
+                              "NCOPIES times along a specified dimension DIM.", "SOURCE,DIM,NCOPIES"),
         fortran_intrinsic_obj("SQRT", 3, "SQRT(X) computes the square root of X.", "X"),
         fortran_intrinsic_obj("SUM", 3, "SUM(ARRAY,DIM=dim,MASK=mask) adds the elements of ARRAY along "
                               "dimension DIM if the corresponding element in MASK is TRUE.",
                               "ARRAY,DIM=dim,MASK=mask"),
+        fortran_intrinsic_obj("SYSTEM_CLOCK", 3, "SYSTEM_CLOCK(COUNT=count,COUNT_RATE=count_rate,COUNT_MAX=count_max) "
+                              "determines the COUNT of a processor clock since an unspecified time in the past modulo "
+                              "COUNT_MAX, COUNT_RATE determines the number of clock ticks per second.",
+                              "COUNT=count,COUNT_RATE=count_rate,COUNT_MAX=count_max"),
         fortran_intrinsic_obj("TAN", 3, "TAN(X) computes the tangent of X.", "X"),
         fortran_intrinsic_obj("TANH", 3, "TANH(X) computes the hyperbolic tangent of X.", "X"),
         fortran_intrinsic_obj("TINY", 3, "TINY(X) returns the smallest positive (non zero) number "
                               "in the model of the type of X.", "X"),
+        fortran_intrinsic_obj("TRANSFER", 3, "TRANSFER(SOURCE,MOLD,SIZE=size) interprets the bitwise "
+                              "representation of SOURCE in memory as if it is the representation of a "
+                              "variable or array of the same type and type parameters as MOLD.",
+                              "SOURCE,MOLD,SIZE=size"),
         fortran_intrinsic_obj("TRANSPOSE", 3, "TRANSPOSE(MATRIX) transpose an array of rank two.",
                               "MATRIX"),
         fortran_intrinsic_obj("TRIM", 3, "TRIM(STRING) removes trailing blank characters of a string.",
-                              "STRING")
+                              "STRING"),
+        fortran_intrinsic_obj("UBOUND", 3, "UBOUND(ARRAY,DIM=dim,KIND=kind) returns the upper bounds "
+                              "of an array, or a single upper bound along the DIM dimension.",
+                              "ARRAY,DIM=dim,KIND=kind"),
+        fortran_intrinsic_obj("UPACK", 3, "UPACK(VECTOR,MASK,FIELD) stores the elements "
+                              "of VECTOR in an array of higher rank.", "VECTOR,MASK,FIELD"),
+        fortran_intrinsic_obj("VERIFY", 3, "VERIFY(STRING,SET,BACK=back,KIND=kind) verifies that "
+                              "all the characters in STRING belong to the set of characters in SET.",
+                              "STRING,SET,BACK=back,KIND=kind"),
     ]
